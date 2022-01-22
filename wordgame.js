970f1b81a178bb0709a2af1180727ffbe76b47d4
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2022-01-22 17:43:16 +0200

    Update wordgame.js

--- wordgame.js
+++ wordgame.js
@@ -1,50 +1,35 @@
-window.onload= function (){
-   
-    let words = ["apple", "pear", "peach","mango", "lemon","kiwi"];
-    
-let images = document.getElementsByTagName("img");
-gen(words);
-for (let i = 0 ; i<images.length; i++){
-    images[i].onclick=check
-    function check(event){
-    let image = event.target
-    let word = image.id 
-    let q = prompt("input name of fruit");
-    if (q == words[i]) {
-      alert("Right!");
-    } else {
-      alert("Try again!");
+window.onload = function () {
+  let words = ["apple", "pear", "peach", "mango", "lemon", "kiwi"];
+
+  let images = document.getElementsByTagName("img");
+  gen(words);
+  for (let i = 0; i < images.length; i++) {
+    images[i].onclick = check;
+    function check(event) {
+      let image = event.target;
+      let word = image.id;
+      let q = prompt("input name of fruit");
+      if (q == word) {
+        alert("Right!");
+      } else {
+        alert("Try again!");
+      }
     }
-  
-}
-}
- function gen(words){
-    
-    
- for(let j=0;j<3;j++){
-    document.write('<img id="'+words[getRandomInt(7)]+'" src="img/'+words[getRandomInt(7)]+'.png" width="150">');
- }
-//  for(let k=0;k<1;k++){
-//     document.write('<img id="'+words[getRandomInt(3,5)]+'" src="img/'+words[getRandomInt(3,5)]+'.png" width="150">');
-//  }    
-//  for(let m=0;m<1;m++){
-//     document.write('<img id="'+words[getRandomInt(5,7)]+'" src="img/'+words[getRandomInt(5,7)]+'.png" width="150">');
-//  } 
-    //  function getRandomInt(min,max) {
-    //     return Math.floor(Math.random() * (max - min) + min);
-    //   }
+  }
+  function gen(words) {
+    for (let j = 0; j < 3; j++) {
+      let fruit = getRandomInt(7);
+      document.write(
+        '<img id="' +
+          words[fruit] +
+          '" src="img/' +
+          words[fruit] +
+          '.png" width="150">'
+      );
+    }
 
     function getRandomInt(max) {
-        return Math.floor(Math.random() * max);
-      }
-     
-        
-}
-}
-// for (let i=0;i<words.length;i++){
-   
-
-//     }
-// for (let i=0;i<words.length;i++){
-//     document.write('<img id="'+words[i]+'" src="img/'+words[i]+'.png" width="150">');
-// }
+      return Math.floor(Math.random() * max);
+    }
+  }
+};
